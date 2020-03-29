30d86424446c36a3b61a22bec21c42f3146b8093
Biletskiy <biletskiyna@gmail.com>
2020-03-29 14:13:17 +0300

    commit test

---
+++ 2)ООП в JavaScript. Объекты, прототипы, наследование/user.js
@@ -0,0 +1,12 @@
+
+let user = {
+    'login': '',
+    'password': '',
+    'validetePassword': function () {
+        if (this.password.length > 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
