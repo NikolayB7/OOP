f273ae95380882872b5cc818fa33f94ad67ca95f
Biletskiy <biletskiyna@gmail.com>
2020-04-08 18:28:16 +0300

    commit

---
+++ 2)ООП в JavaScript. Объекты, прототипы, наследование/dz/tile.js
@@ -0,0 +1,13 @@
+let tile = {
+
+    material: '',
+    type: '',
+    size_x: '',
+    size_y: '',
+    outdoor: '',
+    color: '',
+    cover: '',
+    images: ['pic1', 'pic2'],
+
+    __proto__: goods
+}
