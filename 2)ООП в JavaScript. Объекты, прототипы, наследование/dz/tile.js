55496139d7537a669fee13d25399ae17a6f71d81
NikolayB7 <biletskiyna@gmail.com>
2020-04-09 11:06:28 +0300

    commit 2

--- 2)ООП в JavaScript. Объекты, прототипы, наследование/dz/tile.js
+++ 2)ООП в JavaScript. Объекты, прототипы, наследование/dz/tile.js
@@ -2,12 +2,21 @@ let tile = {
 
     material: '',
     type: '',
-    size_x: '',
-    size_y: '',
+    size_x: 20.5,
+    size_y: 20,
     outdoor: '',
     color: '',
     cover: '',
     images: ['pic1', 'pic2'],
 
-    __proto__: goods
+    __proto__: goods,
+    imperialSize() {
+        return [+(this.size_x * 25.4).toFixed(2), +(this.size_y * 24.4).toFixed(2)]
+    },
+    isSquare() {
+        console.log('isSquare ');
+    }
 }
+
+console.log(tile.imperialSize());
+console.log(tile.isSquare());
